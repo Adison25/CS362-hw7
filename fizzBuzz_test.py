ec4620b1e7de490025a8449b899c8c7d0e9983c7
Adison25 <emerickadison@gmail.com>
2021-05-25 18:07:14 -0700

    test for multiple of 3
    Testing if the number is a multiple of 3 then it should return fizz

--- fizzBuzz_test.py
+++ fizzBuzz_test.py
@@ -3,7 +3,7 @@ import code
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(code.printFB(), 'Fizz')
+        self.assertEqual(code.printFB(3), 'Fizz')
 
 if __name__ == '__main__':
     unittest.main()
