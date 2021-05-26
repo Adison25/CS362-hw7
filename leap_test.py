8079e6ad82004910ec258a3486131253ef4f3407
Adison25 <emerickadison@gmail.com>
2021-05-25 21:26:03 -0700

    Update leap_test.py
    added second test for when the year is divisible by 100

--- leap_test.py
+++ leap_test.py
@@ -4,6 +4,8 @@ import leap
 class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leap.year(3), 'No')
+    def test2(self):
+        self.assertEqual(leap.year(100), 'Yes')
 
 if __name__ == '__main__':
     unittest.main()
