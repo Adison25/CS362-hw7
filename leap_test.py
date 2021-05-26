a70fb5d2b8f9d4a3131f23bf4f9289f138c50948
Adison25 <emerickadison@gmail.com>
2021-05-25 21:32:29 -0700

    Update leap_test.py
    running third test for value of 404

--- leap_test.py
+++ leap_test.py
@@ -5,7 +5,9 @@ class TestCase(unittest.TestCase):
     def test1(self):
         self.assertEqual(leap.year(3), 'No')
     def test2(self):
-        self.assertEqual(leap.year(100), 'Yes')
+       self.assertEqual(leap.year(100), 'Yes')
+    def test3(self):
+        self.assertEqual(leap.year(404), 'Yes')
 
 if __name__ == '__main__':
     unittest.main()
