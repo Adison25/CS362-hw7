3ce1fe706c75af6b6cef1c282f399215f635f513
Adison25 <emerickadison@gmail.com>
2021-05-25 21:18:35 -0700

    Update code.py
    code for running it for 100 numbers with the  correct outputs

--- code.py
+++ code.py
@@ -7,3 +7,16 @@ def printFB(num):
         return 'Fizz'
     else:
         return num
+
+def print100FB():
+    for num in range(100):
+        if num%5 == 0 and num%3 == 0:
+            print("FizzBuzz")
+        elif num%5 == 0:
+            print("Buzz")
+        elif num%3 == 0:
+            print("Fizz")
+        else:
+            print(num)
+
+print100FB()
