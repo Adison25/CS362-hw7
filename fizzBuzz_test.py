0260f670b8e38637f1050abc8fcfaf25a3873d42
Adison25 <emerickadison@gmail.com>
2021-05-25 18:14:45 -0700

    Update fizzBuzz_test.py
    testing if the num is divisible by both 3 and  5

--- fizzBuzz_test.py
+++ fizzBuzz_test.py
@@ -6,6 +6,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(code.printFB(3), 'Fizz')
     def test2(self):
         self.assertEqual(code.printFB(5), 'Buzz')
+    def test3(self):
+        self.assertEqual(code.printFB(15), 'FizzBuzz')
 
 if __name__ == '__main__':
     unittest.main()
