6f40caaed5f0e42fa057bbfb28ad37fd67941e6a
Adison25 <emerickadison@gmail.com>
2021-05-25 21:17:06 -0700

    Update fizzBuzz_test.py
    test for when its neither of the otpions so its just the  number

--- fizzBuzz_test.py
+++ fizzBuzz_test.py
@@ -8,6 +8,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(code.printFB(5), 'Buzz')
     def test3(self):
         self.assertEqual(code.printFB(15), 'FizzBuzz')
+    def test4(self):
+        self.assertEqual(code.printFB(7), 7)
 
 if __name__ == '__main__':
     unittest.main()
