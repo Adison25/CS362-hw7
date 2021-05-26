a2674d86210d16a1ff532439c2e7353019578f2a
Adison25 <emerickadison@gmail.com>
2021-05-25 21:30:41 -0700

    Update leap.py
    Finish of leap code so that the second test could run successfully

--- leap.py
+++ leap.py
@@ -1,3 +1,9 @@
 def year(year):
     if year % 4 != 0:
         return "No"
+    elif year % 100 == 0:
+        return "Yes"
+    elif year % 400 != 0:
+        return "No"
+    else:   
+        return "Yes"
